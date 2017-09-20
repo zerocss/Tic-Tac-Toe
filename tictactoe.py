f4aea1e36fdd8066788a24e8d59a18b9ece1d229
Chris Shultz <chris.s.shultz@gmail.com>
2017-09-19 20:03:08 -0400

    Changed from 2 player to an AI opponent

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,5 @@
+import random
+
 player_1 = "X" #Player one is X
 player_2 = "O" #Player two is O
 game = True
@@ -27,6 +29,14 @@ def print_board(board):
     print("---+---+---")
     print(" %s | %s | %s " %(board[7], board[8], board[9]))
     print("")
+    
+#Duplicate board for the computer to use
+def copy_board(board):
+    dup_board = []
+    
+    for position in board:
+        dup_board.append(position)
+    return dup_board
 
 board = [" "] * 10 #Fill board with blanks
 
@@ -64,7 +74,51 @@ def player_move(board):
         move = int(input("Choose a space: (1-9) "))
         print("")
     return move
-
+    
+#Chooses a move for the computer based on board position availability
+def random_move(board, moves):
+    open_positions = []
+    for move in moves:
+        if check_space(board, move):
+            open_positions.append(move)
+            
+    #Check if there are actual moves to make
+    if len(open_positions) != 0:
+        return random.choice(open_positions)
+    else:
+        return None
+        
+#Computer's move
+def comp_move(board):
+    
+    #See if the computer can win in the next move
+    for move in range(1,10):
+        copy = copy_board(board)
+        if check_space(copy, move):
+            place_move(copy, player_2, move)
+            if check_win(copy, player_2):
+                return move
+                
+    #See if player 1 could win in the next move. Block them if necessary
+    for move in range(1,10):
+        copy = copy_board(board)
+        if check_space(copy, move):
+            place_move(copy, player_1, move)
+            if check_win(copy, player_1):
+                return move
+    #Move to the middle if it's free
+    if check_space(board, 5):
+        return 5
+        
+    #Choose a corner if it's free
+    move = random_move(board, [1,3,7,9])
+    if move != None:
+        return move
+        
+    #Choose a side
+    return random_move(board, [2,4,6,8])
+    
+    
 #Reset board
 def reset_board():
     board = [" "] * 10
@@ -91,15 +145,18 @@ while game:
         else:
             if board_check(board):
                 print_board(board)
-                print("The game is a tie!")
+                print("----------------------")
+                print("| The game is a tie! |")
+                print("----------------------")
+                break
             else:
                 turn = "p2"
     
     else: #Player two
         
         print_board(board)
-        print("Player two's move: ")
-        move = player_move(board)
+        print("The Computer's turn: ")
+        move = comp_move(board)
         place_move(board, player_2, move)
         
         if check_win(board, player_2):
@@ -111,6 +168,9 @@ while game:
         else:
             if board_check(board):
                 print_board(board)
-                print("The game is a tie!")
+                print("----------------------")
+                print("| The game is a tie! |")
+                print("----------------------")
+                break
             else:
                 turn = "p1"
